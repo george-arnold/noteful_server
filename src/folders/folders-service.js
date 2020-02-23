20df187a021eb2f98f4a9d4cfa22cca7ac0d77d1
George Arnold <george.arnold.88@gmail.com>
2020-02-23 16:57:21 -0500

    initial commit

---
+++ src/folders/folders-service.js
@@ -0,0 +1,34 @@
+const FoldersService = {
+  getAllFolders(knex) {
+    return knex.select("*").from("folders");
+  },
+  getByID(knex, id) {
+    return knex
+      .from("folders")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  insertFolder(knex, newFolder) {
+    return knex
+      .insert(newFolder)
+      .into("folders")
+      .returning(
+        "*".then(rows => {
+          return rows[0];
+        })
+      );
+  },
+  deleteFolder(knex, id) {
+    return knex("folders")
+      .where([id])
+      .delete();
+  },
+  updateBookmark(knex, id, newFolderField) {
+    return knex("folders")
+      .where({ id })
+      .update(newFolderField);
+  }
+};
+
+module.exports = FoldersService;
