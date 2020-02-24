6521b4bfc492a356ab362a09b900ec172e0acede
George Arnold <george.arnold.88@gmail.com>
2020-02-24 07:43:12 -0500

    ran prettier

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,31 +1,31 @@
 const FoldersService = {
   getAllFolders(knex) {
-    return knex.select("*").from("folders");
+    return knex.select('*').from('folders');
   },
   getByID(knex, id) {
     return knex
-      .from("folders")
-      .select("*")
-      .where("id", id)
+      .from('folders')
+      .select('*')
+      .where('id', id)
       .first();
   },
   insertFolder(knex, newFolder) {
     return knex
       .insert(newFolder)
-      .into("folders")
+      .into('folders')
       .returning(
-        "*".then(rows => {
+        '*'.then(rows => {
           return rows[0];
         })
       );
   },
   deleteFolder(knex, id) {
-    return knex("folders")
+    return knex('folders')
       .where([id])
       .delete();
   },
   updateFolder(knex, id, newFolderField) {
-    return knex("folders")
+    return knex('folders')
       .where({ id })
       .update(newFolderField);
   }
